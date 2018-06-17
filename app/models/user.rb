f9ee8892908bc28822a7857d9f4db9ce1cd71977
chrisjamesr <chrisjames.r@gmail.com>
2018-06-17 16:25:41 -0400

    add associations, validations and custom validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :projects
+  has_many :tasks, through: :projects
+  has_many :clients, through: :projects
+
+  validates :name, :email, :presence=> true
+  validates :name, :email, :uniqueness => { :case_sensitive=> false }
+  validates :password, :presence => true, :length => { in: 6..20 }
+  validate :is_valid_email
+
+
+  def is_valid_email
+    #check for valid email address
+    if !!email.match(/.+[@].+[.].+/).nil?
+      errors.add(:email, "Please enter valid email address")
+    end
+  end
 end
