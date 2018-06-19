529370df5f69538cda37c83cd5e63f087ae8c2ed
chrisjamesr <chrisjames.r@gmail.com>
2018-06-19 18:09:13 -0400

    remove protect from forgery, ActionController:API doesn't come with csrf protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :null_session, if: Proc.new {|c| c.request.format.json? }
-  # def current_user
-  #   @user = User.find_by(:id => session[:user_id]) unless nil
-  # end
+  
   
 end
