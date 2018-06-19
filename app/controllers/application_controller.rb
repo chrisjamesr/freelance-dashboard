bf13ba30ac07073a2cfcf4add1146c4c800b79f4
chrisjamesr <chrisjames.r@gmail.com>
2018-06-19 13:05:57 -0400

    comment out current_user method. No session data, cannon be used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   protect_from_forgery with: :null_session, if: Proc.new {|c| c.request.format.json? }
-  def current_user
-    @user = User.find_by(:id => session[:user_id]) unless nil
-  end
+  # def current_user
+  #   @user = User.find_by(:id => session[:user_id]) unless nil
+  # end
   
 end
