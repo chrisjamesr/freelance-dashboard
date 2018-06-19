b8e7d0098f04534e4126544a6a20fbcc160fb01e
chrisjamesr <chrisjames.r@gmail.com>
2018-06-19 13:05:26 -0400

    disable csrf protection for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-
+  protect_from_forgery with: :null_session, if: Proc.new {|c| c.request.format.json? }
   def current_user
     @user = User.find_by(:id => session[:user_id]) unless nil
   end
