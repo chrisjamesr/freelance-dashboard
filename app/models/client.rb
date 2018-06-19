6f4e2c85e2055fa7693acad327b1cb8b22f31afc
chrisjamesr <chrisjames.r@gmail.com>
2018-06-19 13:09:35 -0400

    add associations and validations for client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,7 @@
 class Client < ApplicationRecord
+  has_many :projects
+  has_many :users, through: :projects
+  
+  validates :name, :presence=> true
+  validates :name, :uniqueness => { :case_sensitive=> false }
 end
