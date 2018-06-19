e767b7b2e611e05d0883d043935fd86f91d0810a
chrisjamesr <chrisjames.r@gmail.com>
2018-06-19 19:08:51 -0400

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  resources :users, :only => [:create, :index, :show, :update]
+  resources :users, :only => [:create, :show,:index, :update]
 
   resources :clients , :only => [:create, :show, :update]
 
