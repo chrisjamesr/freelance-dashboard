d5ed4fd57874315e8b894fcd97b8bb2712f294a4
chrisjamesr <chrisjames.r@gmail.com>
2018-06-19 13:19:03 -0400

    add create and destroy action for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+   def create
+    user = User.find(params[:id])
+    if user && user.authenticate(params[:user][:password])
+      set_session
+      redirect_to user_path(current_user)
+    else
+      render json: user.errors
+    end
+
+  end
+
+  def destroy
+    reset_session
+  end
+  
 end
