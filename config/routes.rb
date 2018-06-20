da3dbbccbf2fe8d17835d2b110c40e8dd1104245
chrisjamesr <chrisjames.r@gmail.com>
2018-06-19 21:22:18 -0400

    comment out client routes. not being used

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     resources :users, :except => [:new, :edit, :destroy] do
-      resources :clients , :except => [:new, :edit, :destroy]
+      # resources :clients , :except => [:new, :edit, :destroy]
       resources :projects, :except => [:new, :edit]
       resources :tasks, :except => [:new, :edit]
       resources :notes, :except => [:new, :edit]
